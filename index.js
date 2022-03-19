a030b063783dbd507bd9754f54220d79036aa5a8
1neye <illya.landar@gmail.com>
2022-03-20 00:55:06 +0200

    get crypto pairs list

---
+++ index.js
@@ -0,0 +1,33 @@
+const puppeteer = require(`${__dirname}/modules/puppeteer`);
+const UserAgent = require(`${__dirname}/modules/userAgent`);
+const prompt = require(`${__dirname}/modules/prompt`);
+
+const getcryptoPairs = require(`${__dirname}/modules/getcryptoPairs`);
+
+
+
+
+    // (async () => {
+    //     const browser = await puppeteer.launch({
+    //         headless: false,
+    //     });
+    //     const page = await browser.newPage();
+    //     const userAgent = new UserAgent();
+    //     await page.setUserAgent(userAgent.toString())
+    //     await page.goto('https://2ip.ru/');
+    //     await page.waitForTimeout(3000)
+    //     await page.screenshot({ path: 'example2.png' });
+
+    //     await browser.close();
+    // })();
+
+
+
+let start = async () => {
+    let balanace = 100 // usdt
+
+    let cryptoList= await getcryptoPairs()
+
+}
+
+start()
---
+++ modules/getcryptoPairs.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const fs = require('fs')
+
+const getcryptoPairs = async () => {
+    let res = await axios.get('https://api.binance.com/api/v3/exchangeInfo')
+
+    let pairsList = []
+
+    res.data.symbols.forEach( el => {
+        pairsList.push({baseAsset: el.baseAsset, quoteAsset: el.quoteAsset, symbol: el.symbol})
+    });
+
+    fs.writeFileSync( './data/cryptoList.json', JSON.stringify(pairsList))
+
+    return pairsList
+}
+
+module.exports = getcryptoPairs
